41a46f238f5dfc4193f5bed81cca3832ba683e3c
milesruffin <miles.ruffin@atlasschool.com>
2024-05-20 21:16:38 +0000

    Task 5 hotfix

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
-
 /**
- * main - program that prints the alphabet in lowercase and upper case
+ * main - Prints all single digit numbers of base 10 starting from 0.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char letter;
+	int num;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
 
-	{
-	if (letter != 'e' && letter != 'q')
-	putchar(letter);
-	}
-
-	putchar('\n');
+	printf("\n");
 
 	return (0);
 }
