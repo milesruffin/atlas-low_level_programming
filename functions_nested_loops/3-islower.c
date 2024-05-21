d955c1db9e50283869053c24a6de3d6a39e4088d
milesruffin <miles.ruffin@atlasschool.com>
2024-05-21 00:34:27 +0000

    Task hotfixes

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -18,25 +18,3 @@ int _islower(int c)
 	}
 }
 
-
-3-main.c
-#include "main.h"
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
