7601ba14cd4d218ede2d82503bcf8d0eb2e9d9b9
milesruffin <miles.ruffin@atlasschool.com>
2024-05-21 01:15:53 +0000

    Task 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,19 +1,12 @@
-#include <stdio.h>
-#include <math.h>
-#include <ctype.h>
 #include "main.h"
+
 
 /**
  * add - Adds two values
- *
- *Description: function that adds two integers and
- * returns the result.
- *
  * @n1: integer one
  * @n2: integer two
  * Return: Integer value
  */
-
 
 int add(int n1, int n2)
 
