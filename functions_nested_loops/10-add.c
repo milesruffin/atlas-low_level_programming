26bce67c20e0664d1e70ec8188a18b53fd9f5864
milesruffin <miles.ruffin@atlasschool.com>
2024-05-21 00:42:12 +0000

    Task hotfixes

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -23,24 +23,3 @@ int add(int n1, int n2)
 	sum = n1 + n2;
 	return (sum);
 }
-
-
-
-
-10-main.c
-#include "main.h"
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
-
-
-	n = add(89, 9);
-	printf("%d\n", n);
-	return (0);
-}
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -40,17 +40,3 @@ void times_table(void)
 	_putchar('\n');
 	}
 }
-
-
-9-main.c
-#include "main.h"
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	times_table();
-	return (0);
-}
