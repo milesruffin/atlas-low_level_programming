1befd34d26ac9f2c14ca1fd8f111da1ced65f0fd
milesruffin <miles.ruffin@atlasschool.com>
2024-05-21 01:51:25 +0000

    Task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints the size of various types
- * Return: 0
+ * main - void
+ *
+ * Description: Program that prints sizeof various data types
+ *
+ * @: no parameters
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
-return (0);
+	return (0);
 }
