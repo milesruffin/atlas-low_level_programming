59569606826a46211664727f7c7115463e41fea8
milesruffin <miles.ruffin@atlasschool.com>
2024-05-27 18:46:15 +0000

    Task 5 hotfix

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,12 @@ int count = 0
 
 for (i = 0; s[i] != '\0'; i++)
 	count++;
-for (i = 0; i , count /2; i++)
+for (i = 0; i <  count /2; i++)
 {
 char j;
 j = s[i];
 	s[i] = s[count - 1 - i];
-	s[count -1 -i] = j;
+	s[count - 1 - i] = j;
 }
 
 
