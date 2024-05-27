aecff210ecec243cf16d3ebc18abbe434c944b57
milesruffin <miles.ruffin@atlasschool.com>
2024-05-27 18:08:51 +0000

    Prototype hotfix

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,6 +13,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int putchar (int character)
+int _putchar (char c);
 
 #endif
